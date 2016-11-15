91b9a2eec6756062a52fe46548cdd6ab49f318b0
damsu <damorizur@gmail.com>
2016-11-15 18:00:02 +0200

    Showing list of ringtones (for trst purposes) and adding buttons to each item (work in progress)

---
+++ app/src/main/java/group1/oamk/ringo/getRingtones.java
@@ -0,0 +1,140 @@
+package group1.oamk.ringo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class getRingtones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_ringtones);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ListView lv = (ListView) findViewById(R.id.ringtone_list);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        MyCustomAdapter adapter = new MyCustomAdapter(getRingtones(), this);
+              lv.setAdapter(adapter);
+
+    }
+
+    public class MyCustomAdapter extends BaseAdapter implements ListAdapter {
+        private ArrayList<String> list = new ArrayList<String>();
+        private Context context;
+
+
+
+        public MyCustomAdapter(ArrayList<String> list, Context context) {
+            this.list = list;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return list.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return 0;
+            //just return 0 if your list items do not have an Id variable.
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.content_get_ringtones, null);
+            }
+
+            //Handle TextView and display string from your list
+            TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+            listItemText.setText(list.get(position));
+
+            //Handle buttons and add onClickListeners
+            Button deleteBtn = (Button)view.findViewById(R.id.delete_btn);
+            Button setBtn = (Button)view.findViewById(R.id.set_btn);
+
+            deleteBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    //do something
+                    list.remove(position); //or some other task
+                    notifyDataSetChanged();
+                }
+            });
+            setBtn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    //do something
+                    notifyDataSetChanged();
+                }
+            });
+
+            return view;
+        }
+    }
+
+    public ArrayList<String> getRingtones() {
+        RingtoneManager manager = new RingtoneManager(this);
+        manager.setType(RingtoneManager.TYPE_RINGTONE);
+        Cursor cursor = manager.getCursor();
+
+        ArrayList<String> list = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(RingtoneManager.ID_COLUMN_INDEX);
+            String title = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX);
+
+            list.add(id + " : " + title);
+        }
+
+        return list;
+    }
+
+    public void getNotifications() {
+        RingtoneManager ringtoneManager = new RingtoneManager(this);
+        ringtoneManager.setType(RingtoneManager.TYPE_RINGTONE);
+        Cursor cursor = ringtoneManager.getCursor();
+        while (cursor.moveToNext()) {
+            System.out.print(cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX));
+            System.out.print(cursor.getString(RingtoneManager.URI_COLUMN_INDEX));
+        }
+    }
+
+}
+
