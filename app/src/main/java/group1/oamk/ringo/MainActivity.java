62f5802c5175f0cc02a2e0f280066cdd49463344
Jere Komminaho <jereakom@gmail.com>
2016-11-15 11:06:32 +0200

    Main activity creation and tests

--- app/src/main/java/group1/oamk/ringo/MainActivity.java
+++ app/src/main/java/group1/oamk/ringo/MainActivity.java
@@ -2,6 +2,9 @@ package group1.oamk.ringo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CharSequence test = "asdasdasd";
+                TextView testy = (TextView)findViewById(R.id.test);
+                testy.setText(test);
+            }
+        });
+
     }
+
+    public void testThis(View view) {
+        CharSequence test = "THIS IS ANOTHER TEST";
+        TextView testy = (TextView)findViewById(R.id.test);
+        testy.setText(test);
+    }
+
 }
+
